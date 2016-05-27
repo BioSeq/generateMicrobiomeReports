69e572e9f4f5f1130cab9d2f3e11a841c32a115c
Hannah Voelker <hannahvoelker13@gmail.com>
2016-05-27 11:05:49 -0400

    Add files via upload

---
+++ makepiechart.R
@@ -0,0 +1,51 @@
+
+#!/usr/bin/env Rscript
+#
+# makepiechart.r
+# Author: Hannah Voelker
+#Last modified: 5/5/16
+#Adapted from printPieChart.r
+# Given a set of the 10 most prevalent bacteria in a sample, creates a corresponding pie chart 
+#
+
+#import
+require(plotrix)
+
+# get the csv file
+genuscounts <<- read.csv(file.choose(), header = TRUE, sep = ",")
+
+#loop through the data frame and find the top ten for each 
+for(i in 2:ncol(genuscounts)) {
+    thisColumn <- genuscounts[,c(1,i)]
+    topten <- head(thisColumn[order(thisColumn[,2], decreasing = TRUE), ], 10)
+    totalcount = sum(genuscounts[,i])
+    slices = topten[,2]/totalcount
+    
+  # add labels
+  slices = round(slices*100,2)
+  lbls <- topten[,1]
+  lbls = paste(lbls,as.character(slices))
+  lbls = paste(lbls,"%",sep="")
+
+  # output and aesthetics 
+  outputDir = "Users/hannahvoelker/Documents/BioSeq"
+  samplename = names(genuscounts[i])
+  #remove punct from sample name
+  samplename <- gsub("[[:punct:]]","", samplename)
+  filename = paste(outputDir, as.character(samplename), ".png", sep ="")
+  colors = rainbow(length(slices))
+  pie(slices, '', radius = 0.6, col=colors, main=samplename, cex.main=5, mai=c(0.2,0,2,0.2),family="serif")
+  # can use font.main = '' to adjust the font of main title
+  x= 0.8
+  y= 0.6
+  legend(x,y, lbls, cex=3.6, fill=colors)
+  textX = 0
+  textY = 0.95
+  text(textX,textY,'This sample\'s microbiome, broken down by genus:', cex=4.7, font=4, family="serif")
+  textX = -1.6
+  textY = 0
+  text(textX,textY,' DISCLAIMER:\n Results cannot be\n used to draw any\n conclusions about\n health information.', cex=4, adj=c(0,NA), font=3, family="serif")
+  dev.off()
+  png(file = filename, height=1200, width=2200)
+} 
+# this is the end of the for loop
